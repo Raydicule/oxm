374f55e46f4bd8576ad3b7dd338eb8c4ca7d412a
Ennovate Now <anish.thomas@evolvussolutions.com>
2013-05-26 10:55:51 +0530

    Cleaned out indentation
    Cleaned indentation

--- file-one/src/main/java/com/evolvus/examples/camel/fileOne/App.java
+++ file-one/src/main/java/com/evolvus/examples/camel/fileOne/App.java
@@ -5,21 +5,20 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class App {
-	public static void main(String[] args) throws Exception {
-		final String sourceFolder = "file:data/inbox";
-		final String destinationFolder = "file:data/outbox";
+    public static void main(final String[] args) throws Exception {
+        final String sourceFolder = "file:data/inbox";
+        final String destinationFolder = "file:data/outbox";
 
-		CamelContext camelContext = new DefaultCamelContext();
-		camelContext.addRoutes(new RouteBuilder() {
-			public void configure() {
-				from(sourceFolder).to(destinationFolder);
-			}
-		});
+        final CamelContext camelContext = new DefaultCamelContext();
+        camelContext.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() {
+                from(sourceFolder).to(destinationFolder);
+            }
+        });
 
-		camelContext.start();
-		Thread.sleep(30000);
-		camelContext.stop();
-
-		System.out.println("camel route executed!");
-	}
+        camelContext.start();
+        Thread.sleep(30000);
+        camelContext.stop();
+    }
 }
